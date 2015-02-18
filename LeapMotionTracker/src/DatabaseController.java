46a43e2f3f99151d04e68c6e0c9520c94bebfc4d
Bryan Muscedere <bmuscede@uwo.ca>
2015-02-18 18:56:37 -0500

    Created new metrics average calculations.

--- LeapMotionTracker/src/DatabaseController.java
+++ LeapMotionTracker/src/DatabaseController.java
@@ -513,6 +513,134 @@ public class DatabaseController extends Thread {
 			e.printStackTrace();
 			return;
 		}
+		
+		//Gets the updated average.
+		calculateAverage(skillLevelID, computedHandMotions, computedLeftMotions, computedRightMotions,
+						 computedHandVelocity, computedLeftVelocity, computedRightVelocity);
+	}
+
+	/**
+	 * This method updates the average for the current skill level. It
+	 * checks to see if such a skill level exists and then updates the 
+	 * rolling average.
+	 * @param skillLevelID The skill level ID.
+	 * @param computedHandMotions The hand motion data.
+	 * @param computedLeftMotions The left hand data.
+	 * @param computedRightMotions The right hand data.
+	 * @param computedHandVelocity The hand velocity.
+	 * @param computedLeftVelocity The left velocity.
+	 * @param computedRightVelocity The right velocity.
+	 */
+	private void calculateAverage(int skillLevelID, int[] computedHandMotions,
+			int[] computedLeftMotions, int[] computedRightMotions,
+			float[] computedHandVelocity, float[] computedLeftVelocity,
+			float[] computedRightVelocity) {
+		//First, gets past data.
+		String sql = "SELECT * FROM MetricAverage WHERE SkillID = " + skillLevelID + ";";
+		Vector<Vector<String>> results = getData(sql);
+		
+		//Check to see if an entry exists.
+		if (results.size() == 1 || results.elementAt(1) == null){
+			createNewAverage(skillLevelID, computedHandMotions, computedLeftMotions, computedRightMotions,
+					computedHandVelocity, computedLeftVelocity, computedRightVelocity);
+			return;
+		}
+		
+		//Now, gets the divide value.
+		int divideVal = Integer.parseInt(results.elementAt(1).elementAt(1)) + 1;
+		Vector<String> values = results.elementAt(1);
+		
+		//We generate our SQL
+		sql = "UPDATE SessionMetrics SET " +
+				"NumReference = " + divideVal + ", " +
+				"LeftMotions = " + ((Float.parseFloat(values.elementAt(2)) + computedHandMotions[0]) / divideVal) + ", " +
+				"LeftThumbMotions = " + ((Float.parseFloat(values.elementAt(3)) + computedLeftMotions[0]) / divideVal) + ", " +
+				"LeftIndexMotions = " + ((Float.parseFloat(values.elementAt(4)) + computedLeftMotions[1]) / divideVal) + ", " +
+				"LeftMiddleMotions = " + ((Float.parseFloat(values.elementAt(5)) + computedLeftMotions[2]) / divideVal) + ", " +
+				"LeftRingMotions = " + ((Float.parseFloat(values.elementAt(6)) + computedLeftMotions[3]) / divideVal) + ", " +
+				"LeftPinkyMotions = " + ((Float.parseFloat(values.elementAt(7)) + computedLeftMotions[4]) / divideVal) + ", " +
+				"RightMotions = " + ((Float.parseFloat(values.elementAt(8)) + computedHandMotions[1]) / divideVal) + ", " +
+				"RightThumbMotions = " + ((Float.parseFloat(values.elementAt(9)) + computedRightMotions[0]) / divideVal) + ", " +
+				"RightIndexMotions = " + ((Float.parseFloat(values.elementAt(10)) + computedRightMotions[1]) / divideVal) + ", " +
+				"RightMiddleMotions = " + ((Float.parseFloat(values.elementAt(11)) + computedRightMotions[2]) / divideVal) + ", " +
+				"RightRingMotions = " + ((Float.parseFloat(values.elementAt(12)) + computedRightMotions[3]) / divideVal) + ", " +
+				"RightPinkyMotions = " + ((Float.parseFloat(values.elementAt(13)) + computedRightMotions[4]) / divideVal) + ", " +
+				"LeftVelocity = " + ((Float.parseFloat(values.elementAt(14)) + computedHandVelocity[0]) / divideVal) + ", " +
+				"LeftThumbVelocity = " + ((Float.parseFloat(values.elementAt(15)) + computedLeftVelocity[0]) / divideVal) + ", " +
+				"LeftIndexVelocity = " + ((Float.parseFloat(values.elementAt(16)) + computedLeftVelocity[1]) / divideVal) + ", " +
+				"LeftMiddleVelocity = " + ((Float.parseFloat(values.elementAt(17)) + computedLeftVelocity[2]) / divideVal) + ", " +
+				"LeftRingVelocity = " + ((Float.parseFloat(values.elementAt(18)) + computedLeftVelocity[3]) / divideVal) + ", " +
+				"LeftPinkyVelocity = " + ((Float.parseFloat(values.elementAt(19)) + computedLeftVelocity[4]) / divideVal) + ", " +
+				"RightVelocity = " + ((Float.parseFloat(values.elementAt(20)) + computedHandVelocity[1]) / divideVal) + ", " +
+				"RightThumbVelocity = " + ((Float.parseFloat(values.elementAt(21)) + computedRightVelocity[0]) / divideVal) + ", " +
+				"RightIndexVelocity = " + ((Float.parseFloat(values.elementAt(22)) + computedRightVelocity[1]) / divideVal) + ", " +
+				"RightMiddleVelocity = " + ((Float.parseFloat(values.elementAt(23)) + computedRightVelocity[2]) / divideVal) + ", " +
+				"RightRingVelocity = " + ((Float.parseFloat(values.elementAt(24)) + computedRightVelocity[3]) / divideVal) + ", " +
+				"RightPinkyVelocity = " + ((Float.parseFloat(values.elementAt(25)) + computedRightVelocity[4]) / divideVal) +
+				" WHERE SkillID = " + skillLevelID + ";";
+		
+		//Executes the generated SQL statement.
+		try {
+			Statement query = conn.createStatement();
+			query.executeUpdate(sql);
+		} catch (SQLException e) {
+			//Just doesn't do it!
+			e.printStackTrace();
+			return;
+		}
+	}
+
+	/**
+	 * Creates a new average if one doesn't exist.
+	 * @param skillLevelID The skill level id.
+	 * @param computedHandMotions The hand motions.
+	 * @param computedLeftMotions The left hand motions.
+	 * @param computedRightMotions The right hand motions.
+	 * @param computedHandVelocity The hand velocity.
+	 * @param computedLeftVelocity The left hand velocity.
+	 * @param computedRightVelocity The right hand velocity.
+	 */
+	private void createNewAverage(int skillLevelID, int[] computedHandMotions,
+			int[] computedLeftMotions, int[] computedRightMotions,
+			float[] computedHandVelocity, float[] computedLeftVelocity,
+			float[] computedRightVelocity) {
+		//We first need to generate our SQL statement.
+		String sql = "INSERT INTO MetricAverage VALUES(" +
+				skillLevelID + ", 1," +
+				computedHandMotions[0] + ", " + 
+				computedLeftMotions[0] + ", " + 
+				computedLeftMotions[1] + ", " + 
+				computedLeftMotions[2] + ", " + 
+				computedLeftMotions[3] + ", " + 
+				computedLeftMotions[4] + ", " + 
+				computedHandMotions[1] + ", " + 
+				computedRightMotions[0] + ", " + 
+				computedRightMotions[1] + ", " + 
+				computedRightMotions[2] + ", " + 
+				computedRightMotions[3] + ", " + 
+				computedRightMotions[4] + ", " +
+				computedHandVelocity[0] + ", " + 
+				computedLeftVelocity[0] + ", " + 
+				computedLeftVelocity[1] + ", " + 
+				computedLeftVelocity[2] + ", " + 
+				computedLeftVelocity[3] + ", " + 
+				computedLeftVelocity[4] + ", " + 
+				computedHandVelocity[1] + ", " + 
+				computedRightVelocity[0] + ", " + 
+				computedRightVelocity[1] + ", " + 
+				computedRightVelocity[2] + ", " + 
+				computedRightVelocity[3] + ", " + 
+				computedRightVelocity[4] + ");";
+		
+		//Executes the generated SQL statement.
+		try {
+			Statement query = conn.createStatement();
+			query.executeUpdate(sql);
+		} catch (SQLException e) {
+			//Just doesn't do it!
+			e.printStackTrace();
+			return;
+		}
 	}
 
 	/**
@@ -528,6 +656,9 @@ public class DatabaseController extends Thread {
 	private void updateMetrics(String userName, String session, String metricID, int skillID,
 			int[] computedHandMotions, int[] computedLeftMotions, int[] computedRightMotions,
 			float[] computedHandVelocity, float[] computedLeftVelocity, float[] computedRightVelocity){
+		//Remove past average first.
+		removePastAverage(metricID, skillID);
+		
 		//We generate our SQL
 		String sql = "UPDATE SessionMetrics SET " +
 				"SkillID = " + skillID + ", " + 
@@ -566,6 +697,57 @@ public class DatabaseController extends Thread {
 			e.printStackTrace();
 			return;
 		}
+		
+		//Now, updates the average.
+		calculateAverage(skillID, computedHandMotions, computedLeftMotions, computedRightMotions,
+				computedHandVelocity, computedLeftVelocity, computedRightVelocity);
+	}
+
+	/**
+	 * This helper method removes data from the rolling average.
+	 * @param metricID The id of the metric set to remove.
+	 * @param skillID The skill id to remove from.
+	 */
+	private void removePastAverage(String metricID, int skillID) {
+		//Generates an SQL statement for the metrics.
+		String sql = "SELECT * FROM SessionMetrics WHERE MetricID = " + metricID + ";";
+		Vector<String> metricData = getData(sql).elementAt(1);
+		
+		//Generates the SQL for the average.
+		sql = "SELECT * FROM MetricAverage WHERE SkillID = " + skillID + ";";
+		Vector<String> averageData = getData(sql).elementAt(1);
+		
+		//Sets the number of data items.
+		int reference = Integer.parseInt(averageData.elementAt(1));
+		
+		//Generates the update statement.
+		sql = "UPDATE MetricAverage SET " +
+		"NumReference = " + (reference - 1) + ", " +
+		"LeftMotions = " + ((Float.parseFloat(averageData.elementAt(2)) - Float.parseFloat(metricData.elementAt(2))) * reference) + ", " +
+		"LeftThumbMotions = " + ((Float.parseFloat(averageData.elementAt(3)) - Float.parseFloat(metricData.elementAt(3))) * reference) + ", " +
+		"LeftIndexMotions = " + ((Float.parseFloat(averageData.elementAt(4)) - Float.parseFloat(metricData.elementAt(4))) * reference) + ", " +
+		"LeftMiddleMotions = " + ((Float.parseFloat(averageData.elementAt(5)) - Float.parseFloat(metricData.elementAt(5))) * reference) + ", " +
+		"LeftRingMotions = " + ((Float.parseFloat(averageData.elementAt(6)) - Float.parseFloat(metricData.elementAt(6))) * reference) + ", " +
+		"LeftPinkyMotions = " + ((Float.parseFloat(averageData.elementAt(7)) - Float.parseFloat(metricData.elementAt(7))) * reference) + ", " +
+		"RightMotions = " + ((Float.parseFloat(averageData.elementAt(8)) - Float.parseFloat(metricData.elementAt(8))) * reference) + ", " +
+		"RightThumbMotions = " + ((Float.parseFloat(averageData.elementAt(9)) - Float.parseFloat(metricData.elementAt(9))) * reference) + ", " +
+		"RightIndexMotions = " + ((Float.parseFloat(averageData.elementAt(10)) - Float.parseFloat(metricData.elementAt(10))) * reference) + ", " +
+		"RightMiddleMotions = " + ((Float.parseFloat(averageData.elementAt(11)) - Float.parseFloat(metricData.elementAt(11))) * reference) + ", " +
+		"RightRingMotions = " + ((Float.parseFloat(averageData.elementAt(12)) - Float.parseFloat(metricData.elementAt(12))) * reference) + ", " +
+		"RightPinkyMotions = " + ((Float.parseFloat(averageData.elementAt(13)) - Float.parseFloat(metricData.elementAt(13))) * reference) + ", " +
+		"LeftVelocity = " + ((Float.parseFloat(averageData.elementAt(14)) - Float.parseFloat(metricData.elementAt(14))) * reference) + ", " +
+		"LeftThumbVelocity = " + ((Float.parseFloat(averageData.elementAt(15)) - Float.parseFloat(metricData.elementAt(15))) * reference) + ", " +
+		"LeftIndexVelocity = " + ((Float.parseFloat(averageData.elementAt(16)) - Float.parseFloat(metricData.elementAt(16))) * reference) + ", " +
+		"LeftMiddleVelocity = " + ((Float.parseFloat(averageData.elementAt(17)) - Float.parseFloat(metricData.elementAt(17))) * reference) + ", " +
+		"LeftRingVelocity = " + ((Float.parseFloat(averageData.elementAt(18)) - Float.parseFloat(metricData.elementAt(18))) * reference) + ", " +
+		"LeftPinkyVelocity = " + ((Float.parseFloat(averageData.elementAt(19)) - Float.parseFloat(metricData.elementAt(19))) * reference) + ", " +
+		"RightVelocity = " + ((Float.parseFloat(averageData.elementAt(20)) - Float.parseFloat(metricData.elementAt(20))) * reference) + ", " +
+		"RightThumbVelocity = " + ((Float.parseFloat(averageData.elementAt(21)) - Float.parseFloat(metricData.elementAt(21))) * reference) + ", " +
+		"RightIndexVelocity = " + ((Float.parseFloat(averageData.elementAt(22)) - Float.parseFloat(metricData.elementAt(22))) * reference) + ", " +
+		"RightMiddleVelocity = " + ((Float.parseFloat(averageData.elementAt(23)) - Float.parseFloat(metricData.elementAt(23))) * reference) + ", " +
+		"RightRingVelocity = " + ((Float.parseFloat(averageData.elementAt(24)) - Float.parseFloat(metricData.elementAt(24))) * reference) + ", " +
+		"RightPinkyVelocity = " + ((Float.parseFloat(averageData.elementAt(25)) - Float.parseFloat(metricData.elementAt(25))) * reference) +
+		" WHERE SkillID = " + skillID + ";";
 	}
 
 	public Vector<String> getSkillLevels() {
@@ -732,5 +914,4 @@ public class DatabaseController extends Thread {
 		
 		return null;
 	}
-
 }
